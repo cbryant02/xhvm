5d43a4f2bec31ec9e8eec1cbfa44373ff3b17cdb
Cole Bryant <cole.z.bryant02@gmail.com>
2020-01-23 17:36:50 -0600

    Richer CLI interface

--- src/main/java/com/github/cbryant02/xhvm/VMTranslator.java
+++ src/main/java/com/github/cbryant02/xhvm/VMTranslator.java
@@ -14,21 +14,22 @@ import java.util.stream.Collectors;
 
 public class VMTranslator {
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+        File workingDir = new File(System.getProperty("user.dir"));
+        System.out.println("Current working directory is " + workingDir.getCanonicalPath());
 
         File dir;
         if (args.length > 0) {
-            dir = new File(args[0]);
+            dir = new File(workingDir, args[0]);
             if (!dir.exists() | !dir.isDirectory()) {
-                System.out.println(dir.getPath() + " is not a valid directory. Exiting.");
+                System.err.println("[ERROR] " + dir.getCanonicalPath() + " is not a valid directory. Exiting.");
                 return;
             }
-        } else dir = promptDirectory();
+        } else dir = promptDirectory(workingDir);
 
         // Prompt for directory and scan for .vm files
         List<File> targets = locateVmFiles(dir);
         if (targets == null || targets.isEmpty()) {
-            System.out.println("No files to process. Exiting.");
+            System.err.println("No files to process. Exiting.");
             return;
         }
 
@@ -41,25 +42,33 @@ public class VMTranslator {
 
         // Parse files
         System.out.print("Writing assembly...");
+        File outputFile = new File(dir.getAbsolutePath() + "/" + dir.getName() + ".asm");
         Parser parser = new Parser(strippedAsm);
-        Translator translator = new Translator(new File(dir.getAbsolutePath() + "/" + dir.getName() + ".asm"));
+        Translator translator = new Translator(outputFile);
         while (parser.hasNext()) {
             parser.advance();
             translator.write(parser.getCurrent());
         }
         translator.close();
         System.out.println(" Done.");
+        System.out.println("Results written to " + outputFile.getCanonicalPath());
     }
 
-    private static File promptDirectory() throws IOException {
+    private static File promptDirectory(File workingDir) throws IOException {
         System.out.print("Enter a directory: ");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String filename = in.readLine();
 
-        File dir = new File(filename);
+        File dir = new File(workingDir, filename);
         if (!dir.exists() || !dir.isDirectory()) {
-            System.out.println("[ERROR] " + filename + " is not a valid directory. Please try again.");
-            return promptDirectory();
+            System.err.println("[ERROR] " + dir.getCanonicalPath() + " is not a valid directory. Please try again.");
+            System.err.flush();
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return promptDirectory(workingDir);
         }
         return dir;
     }
@@ -67,14 +76,14 @@ public class VMTranslator {
     private static List<File> locateVmFiles(File dir) throws IOException {
         File[] files = dir.listFiles();
         if (files == null) {
-            System.out.println("[ERROR] An unexpected I/O error has occurred. Please try again.");
+            System.err.println("[ERROR] An unexpected I/O error has occurred. Please try again.");
             return null;
         }
-        if (files.length < 1) System.out.println("[WARNING] " + dir.getName() + " is an empty directory.");
+        if (files.length < 1) System.err.println("[WARNING] " + dir.getName() + " is an empty directory.");
         List<File> ret = Arrays.stream(files)
                 .filter(file -> file.getName().endsWith(".vm"))
                 .collect(Collectors.toList());
-        if (ret.isEmpty()) System.out.println("[WARNING] " + dir.getName() + " contains no .vm files.");
+        if (ret.isEmpty()) System.err.println("[WARNING] " + dir.getName() + " contains no .vm files.");
         return ret;
     }
 

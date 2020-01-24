8d3415542eea51a5bb72125d4e9cd1cd5f15b745
Cole Bryant <cole.z.bryant02@gmail.com>
2020-01-23 19:07:39 -0600

    Switch to Reflections lib for CommandRegistry class list

--- src/main/java/com/github/cbryant02/xhvm/commands/CommandRegistry.java
+++ src/main/java/com/github/cbryant02/xhvm/commands/CommandRegistry.java
@@ -1,68 +1,37 @@
 package com.github.cbryant02.xhvm.commands;
 
 import com.github.cbryant02.xhvm.VMTranslator;
-import com.github.cbryant02.xhvm.commands.impl.math.AddCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.AndCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.EqualCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.GreaterCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.LessCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.NegateCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.NotCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.OrCommand;
-import com.github.cbryant02.xhvm.commands.impl.math.SubtractCommand;
-import com.github.cbryant02.xhvm.commands.impl.pop.PopCommand;
-import com.github.cbryant02.xhvm.commands.impl.pop.PopPointerCommand;
-import com.github.cbryant02.xhvm.commands.impl.pop.PopStaticCommand;
-import com.github.cbryant02.xhvm.commands.impl.pop.PopTempCommand;
-import com.github.cbryant02.xhvm.commands.impl.push.PushCommand;
-import com.github.cbryant02.xhvm.commands.impl.push.PushConstantCommand;
-import com.github.cbryant02.xhvm.commands.impl.push.PushPointerCommand;
-import com.github.cbryant02.xhvm.commands.impl.push.PushStaticCommand;
-import com.github.cbryant02.xhvm.commands.impl.push.PushTempCommand;
 import org.reflections.Reflections;
 
 import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class CommandRegistry {
-    private static final List<Class<? extends Command>> commandMap = List.of(
-            AddCommand.class,
-            AndCommand.class,
-            EqualCommand.class,
-            GreaterCommand.class,
-            LessCommand.class,
-            NegateCommand.class,
-            NotCommand.class,
-            OrCommand.class,
-            SubtractCommand.class,
-            PushConstantCommand.class,
-            PushPointerCommand.class,
-            PushStaticCommand.class,
-            PushTempCommand.class,
-            PushCommand.class,
-            PopCommand.class,
-            PopPointerCommand.class,
-            PopStaticCommand.class,
-            PopTempCommand.class
-    );
+    private static final Set<Class<? extends Command>> commandClasses;
 
-    private static final List<Class<? extends Command>> warned = new ArrayList<>();
-
-    public static Command getImpl(CommandInfo info) {
-        Class<? extends Command> target = commandMap.parallelStream()
-                .filter(candidate -> {
-                    MatchRules rules = candidate.getDeclaredAnnotation(MatchRules.class);
-                    if (rules == null) {
-                        if (!warned.contains(candidate)) {
-                            System.out.println("[WARNING] No match rules declared for " + candidate.getName() + "!");
-                            warned.add(candidate);
-                        }
+    static {
+        Reflections flex = new Reflections(VMTranslator.class.getPackageName());
+        commandClasses = flex.getSubTypesOf(Command.class)
+                .stream()
+                .filter(cl -> {
+                    Object a = cl.getAnnotation(MatchRules.class);
+                    if (a == null) {
+                        System.err.println("[WARNING] No match rules declared for " + cl.getName() + "!");
                         return false;
                     }
+                    return true;
+                })
+                .collect(Collectors.toCollection(HashSet::new));
+    }
 
+    public static Command getImpl(CommandInfo info) {
+        Class<? extends Command> target = commandClasses.parallelStream()
+                .filter(candidate -> {
+                    MatchRules rules = candidate.getDeclaredAnnotation(MatchRules.class);
                     Pattern argPattern = Pattern.compile(rules.argRegex());
                     Matcher argMatcher = argPattern.matcher(info.arg1);
                     return info.type.equals(rules.type()) && argMatcher.matches();
